ce2564c32812de5c50bf81de7b56704538a4bdee
Gianluca Scopelliti <gianlu.1033@gmail.com>
2021-10-20 17:52:04 +0200

    add sancus-sgx example

---
+++ timing/sancus-sgx/ping/src/lib.rs
@@ -0,0 +1,25 @@
+//@ sm_output(send_ping)
+//@ sm_request(send_ping_req)
+
+//@ sm_input
+pub fn recv_pong(data : &[u8]) {
+    info!(&format!("received pong with data size: {}", data.len()));
+
+    // do nothing (or we can restart the cycle again by sending a new ping)
+}
+
+//@ sm_entry
+pub fn start(data : &[u8]) -> ResultMessage {
+    if data.len() != 2 {
+        error!("input `start` needs 2 bytes as input (packet length)");
+        return failure(ResultCode::IllegalPayload, None);
+    }
+
+    // number of bytes is stored in data
+    let data_size = u16::from_le_bytes([data[0], data[1]]);
+
+    info!(&format!("starting ping-pong with data size: {}", data_size));
+    send_ping(&vec![0; data_size as usize]);
+
+    success(None)
+}
