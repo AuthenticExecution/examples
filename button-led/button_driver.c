382526b45c23e49635d2befec8806f88a393bf1b
gianlu33 <36645341+gianlu33@users.noreply.github.com>
2021-11-09 10:41:34 +0100

    fix button-led examples using latest images (#3)
    * fix button-led-native example
    
    * fix button-led example
    
    * fix button-led

--- button-led/button_driver.c
+++ button-led/button_driver.c
@@ -6,5 +6,6 @@ SM_OUTPUT(button_driver, button_pressed);
 
 SM_INPUT(button_driver, trigger_button_press, data, len) {
   puts("[button_driver] Button has been pressed, sending output");
-  button_pressed(NULL, 0);
+  uint16_t dummy = 0;
+  button_pressed(&dummy, 2);
 }
