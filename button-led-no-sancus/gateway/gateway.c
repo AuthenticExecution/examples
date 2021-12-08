64341a9358a094f028934a8beb50618e73849fbc
Gianluca Scopelliti <gianlu.1033@gmail.com>
2021-12-08 04:56:47 -0800

    self-hosted CI and minor changes

--- button-led-no-sancus/gateway/gateway.c
+++ button-led-no-sancus/gateway/gateway.c
@@ -8,7 +8,7 @@ SM_INPUT(button_pressed, data, data_len) {
     unsigned char fake_data[2] = {1,2};
 
     // toggle LED
-    OUTPUT(toggle_led, fake_data, 2);
+    OUTPUT(toggle_led, NULL, 0);
 
     // increment occurrences on db
     OUTPUT(increment_presses, fake_data, 2);
