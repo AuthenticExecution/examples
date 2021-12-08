5bb4a3c8dd77c0cb550ba45c81ac3084102738e2
Gianluca Scopelliti <gianlu.1033@gmail.com>
2021-12-08 03:48:55 -0800

    fix example no-sancus and add makefiles

--- button-led-no-sancus/gateway/gateway.c
+++ button-led-no-sancus/gateway/gateway.c
@@ -4,9 +4,12 @@ SM_OUTPUT(increment_presses);
 SM_INPUT(button_pressed, data, data_len) {
     DMSG("Remote button has been pressed\n");
 
+    // temporary: get around a bug
+    unsigned char fake_data[2] = {1,2};
+
     // toggle LED
-    OUTPUT(toggle_led, NULL, 0);
+    OUTPUT(toggle_led, fake_data, 2);
 
     // increment occurrences on db
-    OUTPUT(increment_presses, NULL, 0);
+    OUTPUT(increment_presses, fake_data, 2);
 }
