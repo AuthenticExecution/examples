275b900ae255f53bcc3f6178f21a459557a3c6ab
Gianluca Scopelliti <gianlu.1033@gmail.com>
2021-12-08 10:02:09 +0000

    update button-led using new TZ API

--- button-led/gateway/sub.mk
+++
@@ -1,7 +0,0 @@
-global-incdirs-y += include
-srcs-y += controller.c
-srcs-y += spongent.c
-srcs-y +=authentic_execution.c
-
-# To remove a certain compiler flag, add a line like this
-#cflags-template_ta.c-y += -Wno-strict-prototypes
