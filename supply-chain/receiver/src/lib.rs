f6ddde9a03fc87fe7e8bdc6bfa91907205e9435f
Gianluca Scopelliti <gianluca.scopelliti@ericsson.com>
2022-11-25 13:35:59 +0100

    updates

--- supply-chain/receiver/src/lib.rs
+++ supply-chain/receiver/src/lib.rs
@@ -20,8 +20,8 @@ lazy_static! {
         Mutex::new(Vec::new())
     };
 
-    static ref SENSOR_DATA_ID: Mutex<u16> = {
-        Mutex::new(0)
+    static ref SENSOR_METADATA: Mutex<Vec<u8>> = {
+        Mutex::new(Vec::new())
     };
 }
 
@@ -35,100 +35,89 @@ pub fn init(_data : &[u8]) -> ResultMessage {
 }
 
 //@ sm_input
-pub fn start_shipment(data : &[u8]) {
-    authentic_execution::measure_time_ms("start_shipment");
+pub fn start_sensing(data : &[u8]) {
+    let mut sensor_data = SENSOR_DATA.lock().unwrap();
+    let mut sensor_metadata = SENSOR_METADATA.lock().unwrap();
+
     debug!(&format!("Received: {:?}", data));
+    sensor_data.clear();
+    sensor_metadata.clear();
+    sensor_metadata.extend_from_slice(data);
+    authentic_execution::measure_time_ms("START_SENSING");
 }
 
 //@ sm_input
-pub fn end_shipment(data : &[u8]) {
-    authentic_execution::measure_time_ms("end_shipment");
+pub fn end_sensing(data : &[u8]) {
     debug!(&format!("Received: {:?}", data));
+    let mut sensor_data = SENSOR_DATA.lock().unwrap();
+    let mut sensor_metadata = SENSOR_METADATA.lock().unwrap();
+    let aes_key = AES_KEY.lock().unwrap();
+    let mut rsa_key = RSA_KEY.lock().unwrap();
+
+    debug!(&format!("Data len: {}", sensor_data.len()));
+
+    //authentic_execution::measure_time_ms("pre-encrypt");
+    
+    // encrypt all with AES
+    let cipher = match encrypt(
+        &sensor_data, 
+        &aes_key, 
+        &sensor_metadata, 
+        &Encryption::Aes
+    ) {
+        Ok(c)   => c,
+        Err(e)  => {
+            error!(e);
+            return;
+        }
+    };
+
+    //authentic_execution::measure_time_ms("pre-hash");
+
+    // SHA-256 of the encrypted data
+    let mut hasher = Sha256::new();
+    hasher.update(&cipher);
+    let result = hasher.finalize();
+
+    //authentic_execution::measure_time_ms("pre-sign");
+
+    // signature of the hash
+    let mut signature = [0u8; RSA_BITS as usize / 8];
+    match rsa_key.sign(
+        mbedtls::hash::Type::Sha256,
+        &result,
+        &mut signature,
+        &mut Rng
+    ) {
+        Ok(_)   => (),
+        Err(e)  => {
+            error!(e);
+            return;
+        }
+    }
+
+    // symmetric key encryption
+    //TODO use a different key (does it matter?)
+    let mut cipher = vec![0u8; RSA_BITS as usize / 8];
+    match rsa_key.encrypt(
+        &aes_key,
+        &mut cipher,
+        &mut Rng
+    ) {
+        Ok(_)   => (),
+        Err(e)  => {
+            error!(e);
+            return;
+        }
+    }
+
+    sensor_data.clear();
+    authentic_execution::measure_time_ms("END_SENSING");
 }
 
 //@ sm_input
 pub fn receive_sensor_data(data : &[u8]) {
     let mut sensor_data = SENSOR_DATA.lock().unwrap();
-    let mut data_id = SENSOR_DATA_ID.lock().unwrap();
-    let len = data.len();
-
-    if len == 2 {
-        debug!(&format!("Received: {:?}", data));
-        let index = u16::from_le_bytes([data[0], data[1]]);
-
-        if index > 0 {
-            debug!(&format!("Start receiving sensor data. Data ID: {}", index));
-            *data_id = index;
-            authentic_execution::measure_time_ms("START_SENSOR_DATA");
-            sensor_data.clear();
-        } else {
-            debug!("Finished receiving sensor data.");
-            let aes_key = AES_KEY.lock().unwrap();
-            let mut rsa_key = RSA_KEY.lock().unwrap();
-
-            debug!(&format!("Data len: {}", sensor_data.len()));
-
-            //authentic_execution::measure_time_ms("pre-encrypt");
-            
-            // encrypt all with AES
-            let cipher = match encrypt(
-                &sensor_data, 
-                &aes_key, 
-                &data_id.to_be_bytes(), 
-                &Encryption::Aes
-            ) {
-                Ok(c)   => c,
-                Err(e)  => {
-                    error!(e);
-                    return;
-                }
-            };
-
-            //authentic_execution::measure_time_ms("pre-hash");
-
-            // SHA-256 of the encrypted data
-            let mut hasher = Sha256::new();
-            hasher.update(&cipher);
-            let result = hasher.finalize();
-
-            //authentic_execution::measure_time_ms("pre-sign");
-
-            // signature of the hash
-            let mut signature = [0u8; RSA_BITS as usize / 8];
-            match rsa_key.sign(
-                mbedtls::hash::Type::Sha256,
-                &result,
-                &mut signature,
-                &mut Rng
-            ) {
-                Ok(_)   => (),
-                Err(e)  => {
-                    error!(e);
-                    return;
-                }
-            }
-
-            // symmetric key encryption
-            //TODO use a different key (does it matter?)
-            let mut cipher = vec![0u8; RSA_BITS as usize / 8];
-            match rsa_key.encrypt(
-                &aes_key,
-                &mut cipher,
-                &mut Rng
-            ) {
-                Ok(_)   => (),
-                Err(e)  => {
-                    error!(e);
-                    return;
-                }
-            }
-
-            sensor_data.clear();
-            authentic_execution::measure_time_ms("END_SENSOR_DATA");
-        }
-    } else {
-        // actual sensor data
-        debug!(&format!("Received sensor data part with size: {}", len));
-        sensor_data.extend_from_slice(data);
-    }
+    debug!(&format!("Received sensor data part with size: {}", data.len()));
+    sensor_data.extend_from_slice(data);
 }
--- supply-chain/sensor.c
+++ supply-chain/sensor.c
@@ -1,8 +1,6 @@
 #include <sancus/reactive.h>
 
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
 #define PART_SIZE 128
 
@@ -10,23 +8,9 @@ SM_DATA({name}) uint16_t sensor_id = 440;
 SM_DATA({name}) uint16_t shipment_id = 56;
 SM_DATA({name}) uint16_t data_id = 32;
 
-SM_OUTPUT({name}, start_shipment);
-SM_OUTPUT({name}, end_shipment);
+SM_OUTPUT({name}, start_sensing);
+SM_OUTPUT({name}, end_sensing);
 SM_OUTPUT({name}, send_sensor_data);
-
-SM_ENTRY({name}) void trigger_start_shipment(uint8_t* data, size_t len) {
-    //puts("[{name}] starting shipment");
-
-    uint16_t msg[3] = { sensor_id, ++shipment_id, data_id };
-    start_shipment((uint8_t *) msg, 6);
-}
-
-SM_ENTRY({name}) void trigger_end_shipment(uint8_t* data, size_t len) {
-    //puts("[{name}] ending shipment");
-
-    uint16_t msg[3] = { sensor_id, shipment_id, data_id };
-    end_shipment((uint8_t *) msg, 6);
-}
 
 SM_ENTRY({name}) void trigger_start_sensing(uint8_t* data, size_t len) {
     uint16_t i = 0;
@@ -46,19 +30,20 @@ SM_ENTRY({name}) void trigger_start_sensing(uint8_t* data, size_t len) {
       return;
     }
 
+    // start sensing
+    start_sensing(data, len);
+
     // compute number of parts of data, in bytes
     uint16_t num_parts = data_size * 1024 / PART_SIZE;
 
-    // notify that sensing has started, passing the id of the data
-    uint16_t msg[1] = { data_id };
-    send_sensor_data((uint8_t *) msg, 2);
-
     // send sensor data, one part at a time
-    for(i=0; i<num_parts; i++) {
+    for(i=num_parts; i>0; i--) {
       send_sensor_data(sensor_data, PART_SIZE);
     }
 
-    // notify that sensing has ended. i should be equal to zero
-    msg[0] = 0;
-    send_sensor_data((uint8_t *) msg, 2);
+    // end sensing
+    start_sensing(data, len);
+
+    // increment data ID
+    data_id++;
 }
